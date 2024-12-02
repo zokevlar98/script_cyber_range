2374e9d34029d88b9051787658f3ddd2c2113135
zokevlar98 <zqouri11@gmail.com>
2024-12-02 01:44:13 +0100

    done1

--- test/test.sh
+++ test/test.sh
@@ -5,11 +5,15 @@ exec 3< users.txt
 exec 4< passwords.txt
 
 # Read user and password
-while read iuser <&3 && read ipasswd <&4 ; do
+while read -r iuser <&3 && read -r ipasswd <&4 ; do
   # Just print this for debugging
-  printf "\tCreating user: %s with password: %s\n" $iuser $ipasswd
-  # Create the user with adduser (you can add whichever option you like)
-  useradd -m -s /bin/false $iuser
+  printf "\tCreating user: %s with password: %s\n" "$iuser" "$ipasswd"
+  # Create the user with useradd (you can add whichever option you like)
+  sudo useradd -m -s /bin/false "$iuser"
   # Assign the password to the user, passwd must read it from stdin
-  passwd $iuser
+  echo "$iuser:$ipasswd" | sudo chpasswd
 done
+
+# Close file descriptors
+exec 3<&-
+exec 4<&-
