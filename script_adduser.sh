b13d678f742a83fa7d73b45695c4cd9fffae0d5a
zokevlar98 <zqouri11@gmail.com>
2024-12-02 19:30:01 +0100

    debugue

--- script_adduser.sh
+++ script_adduser.sh
@@ -21,37 +21,38 @@ GROUP_NAME="IRIC3"
 # Create group if not exists
 if ! getent group "$GROUP_NAME" > /dev/null 2>&1; then
   sudo groupadd "$GROUP_NAME"
-  echo "Group $GROUP_NAME created."
+  echo "Group $GROUP_NAME created." >> output.txt
 else
-  echo "Group $GROUP_NAME already exists."
+  echo "Group $GROUP_NAME already exists." >> output.txt
 fi
 
 # Create users and add them to the group
 for USERNAME in "${USERNAMES[@]}"; do
   if id "$USERNAME" &>/dev/null; then
-    echo "User $USERNAME already exists."
+    echo "User $USERNAME already exists." >> output.txt
   else
     sudo useradd -m -G "$GROUP_NAME" -s /bin/bash "$USERNAME"
-    echo "$USERNAME:$PASSWORD" | chpasswd
-    echo "User $USERNAME created and added to group $GROUP_NAME with password $PASSWORD."
+    echo "$USERNAME:$PASSWORD" | chpasswd 
+    echo "User $USERNAME created and added to group $GROUP_NAME with password $PASSWORD." >> output.txt
   fi
 done
 
 # Grant sudo privileges to the group without password
 if ! grep -q "^%$GROUP_NAME" /etc/sudoers; then
-  echo "%$GROUP_NAME ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
-  echo "Group $GROUP_NAME granted sudo privileges without password."
+  echo "%$GROUP_NAME ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers >> "output.txt"
+  echo "Group $GROUP_NAME granted sudo privileges without password." >> "output.txt"
 else
-  echo "Group $GROUP_NAME already has sudo privileges."
+  echo "Group $GROUP_NAME already has sudo privileges." >> "output.txt"
 fi
 
 # Restart SSH service
-echo "Restarting SSH service..."
-sudo systemctl restart sshd
+echo "Restarting SSH service..." >> "output.txt"
+sudo systemctl restart sshd >> "output.txt"
+sudo systemctl status sshd >> "output.txt"
 
-# Open SSH port in firewall
-echo "Configuring firewall..."
-sudo ufw allow 22
+# # Open SSH port in firewall
+# echo "Configuring firewall..."
+# sudo ufw allow 22
 sudo ufw reload
 
 # Restore default SSH configuration if needed
@@ -71,6 +72,9 @@ PrintMotd no
 AcceptEnv LANG LC_*
 Subsystem sftp /usr/lib/openssh/sftp-server
 EOL
+
+sudo cat /etc/ssh/sshd_config >> output.txt
+
   sudo sshd -t
   if [ $? -eq 0 ]; then
     sudo systemctl restart sshd
@@ -79,11 +83,9 @@ EOL
     echo "Error in SSH configuration syntax. Please review."
   fi
 
-sudo systemctl restart sshd
-
 # Open SSH port in firewall
 echo "Configuring firewall..."
 sudo ufw allow 22
 sudo ufw reload
-
+sudo systemctl status sshd >> "output.txt"
 echo "Script execution completed."
