b415346240ca23e52c2f4f8e7cf8fd953b907674
Marouane <marouane2002zaaraoui@gmail.com>
2024-12-02 17:11:42 +0100

    Noms

--- script_adduser.sh
+++ script_adduser.sh
@@ -2,17 +2,19 @@
 
 # Variables
 USERNAMES=(
-  "user1" "user2" "user3" "user4" "user5"
-  "user6" "user7" "user8" "user9" "user10"
-  "user11" "user12" "user13" "user14" "user15"
-  "user16" "user17" "user18" "user19" "user20"
-  "user21" "user22" "user23" "user24" "user25"
-  "user26" "user27" "user28" "user29" "user30"
-  "user31" "user32" "user33" "user34" "user35"
-  "user36" "user37" "user38" "user39" "user40"
-  "user41" "user42" "user43" "user44" "user45"
-  "user46" "user47" "user48" "user49" "user50"
+  "AKARID" "ALAOUI-KASMI" "AMSAADI" "AQESRI" "BENNANI"
+  "BOUCHTI" "BOULAMTAT" "CHAHBY" "CHEBLAOUI" "EL ALAOUI"
+  "EL FATHI" "EL HABCHI" "EL HIANI" "EL-ABBOUDY" "EL WADOUDI"
+  "ERAOUI" "EZZINE" "EZZOUITINA" "FARKH" "GHARNATEI"
+  "GOURRAM" "HBILATE" "JAIMOUH" "JDID" "JELAIDI"
+  "KADIRI" "KADOURI" "KAMILI" "LAGRAINI" "LEGNAFDI"
+  "MAJIDI" "MALIKI" "MEROUANE" "MOUINE" "MZIGUIR"
+  "NAJEM" "OUNAIM" "PEZONGO" "QOURI" "RACHDA"
+  "SADANI" "SARSAR" "SAYAH" "TAHIRI ALAOUI" "TALIBI"
+  "TARIK" "TEMRAOUI" "TOUBI" "WADDAY" "WATIK"
+  "ZAAKOUR" "ZAARAOUI"
 )
+
 PASSWORD="cyberrange"
 GROUP_NAME="IRIC3"
 
@@ -53,10 +55,7 @@ sudo ufw allow 22
 sudo ufw reload
 
 # Restore default SSH configuration if needed
-read -p "Do you want to restore the default SSH configuration? (y/n): " RESTORE_CONFIG
-if [[ "$RESTORE_CONFIG" == "y" || "$RESTORE_CONFIG" == "Y" ]]; then
-  echo "Replace default SSH configuration with ower... "
-  cat > /etc/ssh/sshd_config <<EOL
+cat > /etc/ssh/sshd_config <<EOL
 # Default SSH configuration
 Port 22
 AddressFamily any
