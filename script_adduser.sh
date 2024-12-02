cabb6b4a491634987342819dd426d50909b0ad8d
zokevlar98 <zqouri11@gmail.com>
2024-12-02 02:39:09 +0100

    5

--- script_adduser.sh
+++ script_adduser.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+USERNAMES=(
+  "user1" "user2" "user3" "user4" "user5"
+  "user6" "user7" "user8" "user9" "user10"
+  "user11" "user12" "user13" "user14" "user15"
+  "user16" "user17" "user18" "user19" "user20"
+  "user21" "user22" "user23" "user24" "user25"
+  "user26" "user27" "user28" "user29" "user30"
+  "user31" "user32" "user33" "user34" "user35"
+  "user36" "user37" "user38" "user39" "user40"
+  "user41" "user42" "user43" "user44" "user45"
+  "user46" "user47" "user48" "user49" "user50"
+)
+
 # Password for all users
 PASSWORD="cyberrange"
+GROUP_NAME="IRIC3"
 
 # Create the group if it doesn't exist
 if ! getent group "$GROUP_NAME" > /dev/null 2>&1; then
@@ -21,8 +35,13 @@ for USERNAME in "${USERNAMES[@]}"; do
     useradd -m -G "$GROUP_NAME" -s /bin/bash "$USERNAME"
     echo "$USERNAME:$PASSWORD" | chpasswd
     echo "User $USERNAME created and added to group $GROUP_NAME with password $PASSWORD."
+    sudo sed -i 's/^#PasswordAuthentication ./PasswordAuthentication yes/' /etc/ssh/sshd_config
+    sudo sed -i 's/^#PermitRootLogin ./PermitRootLogin no/' /etc/ssh/sshd_config
   fi
 done
+
+# sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+# sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
 # Give the group sudo privileges without password
 if ! grep -q "^%$GROUP_NAME" /etc/sudoers; then
@@ -31,3 +50,5 @@ if ! grep -q "^%$GROUP_NAME" /etc/sudoers; then
 else
   echo "Group $GROUP_NAME already has sudo privileges."
 fi
+
+systemctl restart sshd
